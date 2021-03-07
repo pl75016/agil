7453b78951d5b44d546bb51d996b79a6fc735019
pierre-louis barbier <47251134+pl75016@users.noreply.github.com>
2021-03-07 22:26:44 +0100

    Add files via upload

---
+++ projet/Main.java
@@ -0,0 +1,32 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Iterator;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+public class Main {
+    public static void main (String[] args){
+        try{
+            JSONParser parser = new JSONParser();
+        
+            Object obj = parser.parse(new FileReader("./test.json"));
+
+            JSONObject jsonObject =  (JSONObject) obj;
+
+            String name = (String) jsonObject.get("name");
+            System.out.println(name);
+
+            String city = (String) jsonObject.get("age");
+            System.out.println(city);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
+}
